d174cc99beeb84c890ef4590181ba12eaa5c45b1
Artem <artemlyksa@gmail.com>
2019-05-17 10:27:54 +0300

    Initial commit

---
+++ ImageUploader/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  ImageUploader
+//
+//  Created by Artem Lyksa on 5/17/19.
+//  Copyright © 2019 lyksa. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+    
+}
+
