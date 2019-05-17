f467e2d054d0197f6a27aca7cab9dbf7f9397fbf
Artem <artemlyksa@gmail.com>
2019-05-17 12:43:04 +0300

    Added ImagesListViewController to the project.

---
+++ ImageUploader/Scenes/ImagesListScene/Views/ImagesListViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ImagesListViewController.swift
+//  ImageUploader
+//
+//  Created by Artem Lyksa on 5/17/19.
+//  Copyright © 2019 lyksa. All rights reserved.
+//
+
+import UIKit
+
+class ImagesListViewController: UIViewController {
+    
+    var presenter: ImagesListPresenter!
+    @IBOutlet var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
+extension ImagesListViewController: UICollectionViewDataSource {
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return presenter.assets.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.getCell(ofType: ImageCollectionViewCell.self, for: indexPath)
+        return cell
+    }
+
+}
+
+extension ImagesListViewController: UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+        //TODO: Upload image to "imgur"
+    }
+    
+}
+
+extension ImagesListViewController: UICollectionViewDelegateFlowLayout {
+    
+    
+    private var multiplier: CGFloat {
+        return view.frame.width > view.frame.height ? 5 : 3
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        collectionView.reloadData()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        var width = collectionView.frame.width
+        
+        if let flowLayout = collectionViewLayout as? UICollectionViewFlowLayout {
+            width -= flowLayout.minimumLineSpacing * multiplier
+            width /= multiplier
+        }
+        
+        return CGSize(width: width, height: width)
+    }
+}
